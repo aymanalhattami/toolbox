06657d5f96b56f77299b5275d3a424204258bbbb
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2024-12-26 10:37:32 +0300

    Create EvaluatesClosures.php

---
+++ src/Traits/EvaluatesClosures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aymanalhattami\Toolbox\Traits;
+
+trait EvaluatesClosures
+{
+    public function evaluate($value, array $args = [])
+    {
+        if ($value instanceof \Closure) {
+            return $value(...$args);
+        }
+
+        if (is_callable($value)) {
+            return call_user_func_array($value, $args);
+        }
+
+        return $value;
+    }
+}
